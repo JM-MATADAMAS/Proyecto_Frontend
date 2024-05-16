6a909a4b2ae68656c962d1e27e45dfdfd92f2f28
Javier Matadamas <123751046+JM-MATADAMAS@users.noreply.github.com>
2024-05-16 12:04:56 -0600

    Avance de la pagina principal

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,90 +1,101 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      elevation="0"
+      color="#100F57"
+      absolute
+      height="150px"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-container fluid>
+        <v-row align="center" justify="space-between" no-gutters>
+          <v-col cols="auto">
+            <v-img
+              src="https://s3-alpha-sig.figma.com/img/9a98/bc56/75248163a8e07abbbbb0492141c9529f?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HSUAYoZkh-ibu8r35PUKWN6su2n7-qckZNOFxk5KJdFT7dIlHQdyiUjMzHPXSwEj9b8RJbK6bKDesjazuWCOoe9Vlplo4-WVSiApi502pR9Rrn3Emv3wQ~HOWTzh4UsDHtKwLrBl5UPIkmDH9G68ZskXWLSoc52d-4VU5Ht4eIH8q1U5ks8MT~hKwWuQAPuB9g6sgATUuVL3PBA3CpGFx5OyM4VN5LhVRsVUwRiTKMT8iY94aHjir1wPn1qF8yC7uEUkK3vGg8LDKX-PHVFAkX70SRONw~wM3QoQ5tRfrbjBSd1UG1QuQnk3oqXPUoTu9eDl9opLEUfqCpJ~S~jMhA__"
+              max-height="26"
+              max-width="98px"
+              contain
+              style="margin-left: 151px;"
+            />
+          </v-col>
+          <v-col class="d-none d-md-flex" cols="auto">
+            <v-row align="center" justify="center" no-gutters>
+              <v-btn
+                v-for="(item, i) in items"
+                :key="i"
+                :to="item.to"
+                router
+                exact
+                text
+                plain
+                color="white"
+                class="fuente text-capitalize mx-2"
+              >
+                {{ item.title }}
+              </v-btn>
+            </v-row>
+          </v-col>
+          <v-col class="d-none d-md-flex" cols="auto">
+            <v-btn
+              rounded
+              large
+              elevation="0"
+              color="#0A92DD"
+              class="fuente login-btn"
+              style="margin-right: 121px;"
+            >
+              <span style="color: white; text-transform: capitalize;">
+                Login
+              </span>
+            </v-btn>
+          </v-col>
+          <v-col cols="auto" class="d-md-none">
+            <v-menu offset-y>
+              <template #activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on">
+                  <v-icon color="white">
+                    mdi-menu
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  :to="item.to"
+                  router
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                  <v-btn
+                    rounded
+                    large
+                    elevation="0"
+                    color="#0A92DD"
+                    class="fuente"
+                    style="color: white; width: 100%;"
+                  >
+                    Login
+                  </v-btn>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+      <Nuxt />
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
+    <!--v-footer
       :absolute="!fixed"
       app
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer-->
   </v-app>
 </template>
 
@@ -98,14 +109,28 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Pricing'
+          // , to: '/Pricing'
+        },
+        {
+          title: 'Use Cases'
+          // , to: '/UseCases'
+        },
+        {
+          title: 'Location'
+          // , to: '/Location'
+        },
+        {
+          title: 'FAQ'
+          // , to: '/'
+        },
+        {
+          title: 'Company'
+          // , to: '/'
         }
       ],
       miniVariant: false,
@@ -116,3 +141,18 @@ export default {
   }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap');
+
+.login-btn {
+  white-space: nowrap;
+}
+
+.fuente {
+  font-family: "Karla", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: regular;
+  font-style: normal;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,90 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+  <v-sheet height="815px" max-width="100%" color="#100F57">
+    <div style="display: flex; flex-direction: column; align-items: center;">
+      <v-btn
+        rounded
+        elevation="0"
+        color="rgba(255, 255, 255, 0.05)"
+        style="width: 90%; max-width: 400px; margin-top: 53px; margin-bottom: 16px;"
+        class="fuente"
+      >
+        <span style="color: rgba(216, 153, 37, 1); text-transform: capitalize;">
+          Get your free consultation now
+        </span>
+      </v-btn>
+      <v-card elevation="0" color="#100F57" style="display: flex; flex-direction: column; align-items: center; margin-bottom: 16px;">
+        <v-card-title
+          color="white"
+          class="text-center"
+          style="color: white; font-size: 3vw; font-weight: 600; font-family: 'Kumbh Sans', sans-serif; margin-bottom: 16px; text-align: center;"
+        >
+          Manage your team
         </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+        <v-card-title
+          color="white"
+          class="text-center"
+          style="color: white; font-size: 3vw; font-weight: 600; font-family: 'Kumbh Sans', sans-serif; margin-bottom: 16px; text-align: center;"
+        >
+          easily with task man
+        </v-card-title>
+        <v-card-text
+          color="white"
+          class="text-center fuente"
+          style="color: white; font-size: 1vw;"
+        >
+          Statisdaa is a school management solution that
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+        <v-card-text
+          color="white"
+          class="text-center fuente"
+          style="color: white; font-size: 1vw;"
+        >
+          offers a personalized portal to each type of user,
+        </v-card-text>
       </v-card>
-    </v-col>
-  </v-row>
+      <div style="display: flex; justify-content: center;">
+        <v-btn
+          rounded
+          elevation="0"
+          color="#0A92DD"
+          x-large
+          style="width: 90%; max-width: 400px; margin-bottom: 16px;"
+        >
+          <span style="color: white; text-transform: capitalize;">
+            Get Started
+          </span>
+        </v-btn>
+      </div>
+    </div>
+    <v-img
+      src="https://s3-alpha-sig.figma.com/img/413f/8a42/730fafa4c464bba9da308b1c2112a48e?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=d8CYGb1SWO0JhGoege4J8MESUhV9rHIS6pxPYDis1jbJj9AANPq-9SKBm48VSRgQ7xzbE8gWl4h1KYnwAQ5MsGdNjKsuiQriSFs~uQhDdBk4xCTEaKXEMGg21~pkR5tQafAdmtvSnNkhewt7Nr8qpJddjlj4f6BiYpq9ZkUeLSdAbH2JUk0~~E1UDBdjEDKanHrFxs1i0XsVnEuCByhYhZDluey0oIKmnQ93rbY0v9kpkPDlWZYo0BviIJUh8b8PVuMGl8sl9GI4jAsrtSAzfMt9BVFZY1cHctnoUZ6xnRNn8YprTJ7YhkNBHxmMKcJx0mG8zkiMl37wU4QhoWpk9w__"
+      style="width: 90%; max-width: 931px; margin: 20px auto;"
+      contain
+    />
+    <v-card elevation="0" style="display: flex; flex-direction: column; align-items: center; margin-top: 40px; margin-bottom: 16px;">
+      <v-card-title
+        class="text-center"
+        style=" font-size: 2.5vw; font-weight: 600; font-family: 'Kumbh Sans', sans-serif; margin-bottom: 16px; text-align: center;"
+      >
+        Trusted by company like
+      </v-card-title>
+    </v-card>
+    <div style="display: flex; justify-content: center;">
+      <v-spacer />
+      <img :src="require('@/static/Standard_Collection_24.png')" alt="Image">
+      <v-spacer />
+      <img :src="require('@/static/Standard_Collection_29.png')" alt="Image" style="margin-right: 20px;">
+      <v-spacer />
+      <img :src="require('@/static/Vector.png')" alt="Image" style="margin-right: 20px;">
+      <v-spacer />
+      <img :src="require('@/static/Standard_Collection_24.png')" alt="Image">
+      <v-spacer />
+      <img :src="require('@/static/Standard_Collection_29.png')" alt="Image" style="margin-right: 20px;">
+      <v-spacer />
+      <img :src="require('@/static/Vector.png')" alt="Image" style="margin-right: 20px;">
+      <v-spacer />
+    </div>
+  </v-sheet>
 </template>
 
 <script>
@@ -81,3 +92,12 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+.fuente {
+  font-family: 'Kumbh Sans', sans-serif;
+}
+</style>
